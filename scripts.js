0f2f3dc75007e407d9a29e2e116a3cac8ae8e573
Prabodh-Panda <dev.prabodhpanda@gmail.com>
2021-08-17 10:39:20 +0530

    Added all files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('.hamburger-menu').on('click', function() {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    });
+
+    $('.nav-link').on('click', function() {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    });
+
+    AOS.init({
+        easing: `ease`,
+        duration: 1000,
+    });
+});
